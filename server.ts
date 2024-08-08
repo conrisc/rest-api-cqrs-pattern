3a2ca279671d391502f2fcfb616e2a6f44ec2cd3
Konrad C <conradcielecki@gmail.com>
2024-08-08 18:29:07 +0200

    Init Express app with root endpoint

---
+++ server.ts
@@ -0,0 +1,10 @@
+import { app } from './src'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.PORT ?? 3000
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
