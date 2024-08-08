dd3ac20e7552845e6a625acd6d512790dbe5f9fc
Konrad C <conradcielecki@gmail.com>
2024-08-08 19:49:15 +0200

    Add lowdb and implement LowdbService

---
+++ src/infrastracture/database/lowdb.ts
@@ -0,0 +1,24 @@
+import { Low } from 'lowdb/lib'
+import { JSONFilePreset } from 'lowdb/node'
+import { Product } from '../../products/products.controller'
+
+const DB_FILE_NAME = process.env.DB_FILE_NAME ?? 'db.json'
+
+export interface Schema {
+	products: Product[]
+}
+
+const defaultData: Schema = { products: [] }
+
+export class LowdbService {
+	private static instance: Low<Schema>
+
+	private constructor() {}
+
+	static async getInstance(): Promise<Low<Schema>> {
+		if (!this.instance) {
+			this.instance = await JSONFilePreset(DB_FILE_NAME, defaultData)
+		}
+		return this.instance
+	}
+}
