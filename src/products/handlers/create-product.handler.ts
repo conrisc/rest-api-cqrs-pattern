2179a19eadd49b94419643eca28a2895f07a0281
Konrad C <conradcielecki@gmail.com>
2024-08-11 20:16:45 +0200

    Add validation for CreateProductCommand

--- src/products/handlers/create-product.handler.ts
+++ src/products/handlers/create-product.handler.ts
@@ -7,13 +7,24 @@ export class CreateProductHandler {
 	async execute(command: CreateProductCommand): Promise<string> {
 		console.log('Creating new product. Command: ', command)
 		const db = await LowdbService.getInstance()
-		// TODO: Add validation
+
+		const { name, description, price, stock } = command
+		if (!name) throw Error('"name" is required.')
+		if (!description) throw Error('"description" is required.')
+		if (!price) throw Error('"price" is required.')
+		if (!stock) throw Error('"stock" is required.')
+
+		if (name.length > 50) throw Error('"name" cannot be longer than 50 characters.')
+		if (description.length > 50) throw Error('"description" cannot be longer than 50 characters.')
+		if (price <= 0) throw Error('"price" must be greater than 0.')
+		if (stock <= 0) throw Error('"stock" must be greater than 0.')
+
 		const newProduct: Product = {
 			id: randomUUID(),
-			name: command.name,
-			description: command.description,
-			price: command.price,
-			stock: command.stock,
+			name,
+			description,
+			price,
+			stock,
 		}
 		await db.update(({ products }) => {
 			products.push(newProduct)
